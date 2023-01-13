700d40b1c1a50c957d18d80fe357980ff7bc4df7
Gunho Park <devgun@kakao.com>
2023-01-14 01:13:56 +0900

    add: V_9_2 openssh

---
+++ cent79_2_root/upgrade-openssh92.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update the system
+yum update -y
+
+yum -y install automake
+
+# Download and install OpenSSH
+wget https://github.com/openssh/openssh-portable/archive/refs/tags/V_9_2_P1.tar.gz
+tar xvzf V_9_2_P1.tar.gz
+cd openssh-portable-V_9_2_P1
+
+autoreconf
+./configure && make && make install
+
+# Start the OpenSSH service
+systemctl start ssh
+
+# Enable the OpenSSH service to start automatically on boot
+systemctl enable ssh
+
+ssh -V
